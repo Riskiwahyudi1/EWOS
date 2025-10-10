aa8ec504ca6dcd7fad2cf209de61d3a7fee8d42f
Wahyudi, Riski <Riski.Wahyudi@excelitas.com>
2025-10-10 18:34:33 +0700

    Membuat halaman dahsboard, list mesin, dan list raw material di area Admin Sistem

---
+++ EWOS_MVC/Areas/AdminSystem/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWOS_MVC.Areas.AdminSystem.Controllers
+{
+    [Area("AdminSystem")]
+    public class DashboardController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EWOS_MVC/Areas/AdminSystem/Controllers/RawMaterialController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWOS_MVC.Areas.AdminSystem.Controllers
+{
+    [Area("AdminSystem")]
+    public class RawMaterialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
