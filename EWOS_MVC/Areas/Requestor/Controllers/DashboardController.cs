3d6e477a8b9612179d3912ef4d5f9ef74016f4a1
Wahyudi, Riski <Riski.Wahyudi@excelitas.com>
2025-10-11 14:22:11 +0700

    Membuat tampilan form request fabrikasi baru

---
+++ EWOS_MVC/Areas/Requestor/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWOS_MVC.Areas.Requestor.Controllers
+{
+    [Area("Requestor")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
