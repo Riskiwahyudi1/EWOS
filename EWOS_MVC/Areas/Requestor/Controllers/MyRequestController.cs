8fe1e363475b2df2361ed246dd00107e5069b535
Wahyudi, Riski <Riski.Wahyudi@excelitas.com>
2025-10-17 18:05:41 +0700

    Menambahkan halaman riwayat request fabrikasi

---
+++ EWOS_MVC/Areas/Requestor/Controllers/MyRequestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWOS_MVC.Areas.Requestor.Controllers
+{
+    [Area("Requestor")]
+
+    public class MyRequestController : Controller
+    {
+        [Route("/Requestor/MyRequest/{status?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
