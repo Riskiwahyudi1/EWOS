3177f6b029d19ef563146e3161558661d9b764e1
Wahyudi, Riski <Riski.Wahyudi@excelitas.com>
2025-10-24 18:49:31 +0700

    Menambahkan tampilan history fabrikasi

---
+++ EWOS_MVC/Areas/Requestor/Controllers/FabricationHistoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWOS_MVC.Areas.Requestor.Controllers
+{
+    [Area("Requestor")]
+    public class FabricationHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
