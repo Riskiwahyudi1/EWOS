3a13c524ea7d816b16bd74173b3a8141b1d6fcd6
Wahyudi, Riski <Riski.Wahyudi@excelitas.com>
2025-10-28 15:43:39 +0700

    Membuat halaman approval request

---
+++ EWOS_MVC/Areas/Supervisor/Controllers/ApprovalController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EWOS_MVC.Areas.Supervisor.Controllers
+{
+    [Area("Supervisor")]
+    public class ApprovalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
